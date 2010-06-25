a7fd8961d7d384cc43ea10f344fc5c29465404e4
benoit.siri <benoit.siri@46d0faaf-50e8-9009-db50-26aeffe045a9>
2010-06-25 17:28:43 +0000

    allez hope, on remonte tout ça

---
+++ trunk/PointPausePresentation/src/fr/afcepf/g1/presentation/bean/ApplicationBean.java
@@ -0,0 +1,30 @@
+package fr.afcepf.g1.presentation.bean;
+
+public class ApplicationBean {
+	
+	/*
+	 * todo : ajouter un truc pour document xml
+	 * 
+	 * private String nomBaseFichier
+	 * private XMLmachin fichierLangue
+	 * 
+	 * private loadFichierLangue(String langue){
+	 *  fichierLangue = charger(nomBaseFichier+nomBaseFichier)
+	 * }
+	 * 
+	 */
+	
+	
+	public ApplicationBean(){
+		//trucs à mettre pour vérifier que l'utilisateur est connecté
+		/*
+		 * pseudo code
+		 * 
+		 * 1) verifier si session existe
+		 * 2) si n'existe pas, sortir
+		 * 3) sinon, charger le fichier de langue loadFichierLangue(langue)
+		 * 
+		 * 
+		 */
+	}
+}
