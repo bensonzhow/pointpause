f5766652a404e4d373503f4b93b5583e69676130
benoit.siri <benoit.siri@46d0faaf-50e8-9009-db50-26aeffe045a9>
2010-06-28 10:08:33 +0000

    trop de bordel, je le recréé

--- PointPauseData/src/fr/afcepf/ai77/g1/persistence/interfaces/IDonneesClientDAO.java
+++
@@ -1,8 +0,0 @@
-package fr.afcepf.ai77.g1.persistence.interfaces;
-
-import fr.afcepf.ai77.g1.persistence.dao.ClientDAO;
-
-public interface IDonneesClientDAO {
-	ClientDAO getClientByNumero(int numero);
-	
-}
