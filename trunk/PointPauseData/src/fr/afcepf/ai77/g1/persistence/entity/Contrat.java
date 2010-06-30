12a6eb34c37f99b5e61ec58278f9dcbf4e91448b
benoit.siri <benoit.siri@46d0faaf-50e8-9009-db50-26aeffe045a9>
2010-06-30 19:25:53 +0000

    encore plus d'entités mappées, mais toujours pas eu de test unitaire pour debugger. Attention, hein.

--- trunk/PointPauseData/src/fr/afcepf/ai77/g1/persistence/entity/Contrat.java
+++ trunk/PointPauseData/src/fr/afcepf/ai77/g1/persistence/entity/Contrat.java
@@ -1,5 +1,9 @@
 package fr.afcepf.ai77.g1.persistence.entity;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 
 //FIXME : ce truc n'est qu'un squelette de classe !
 
@@ -14,8 +18,72 @@ package fr.afcepf.ai77.g1.persistence.entity;
 public class Contrat {
 	private Integer numero;
 	private Client client;
+	//private Contrat contrat;
+	private Date dateDebut;
+	private Date dateFin;
+	private Boolean garantie;
+	private Integer freqApprovisionnement;
+	private Date dateSignature;
+	private int duree;
+	private String commentaire;
+	private Boolean flag;
+	private Set<Bouquet> listeBouquets = new HashSet<Bouquet>();
 	
 	
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+	public Date getDateFin() {
+		return dateFin;
+	}
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+	public Boolean getGarantie() {
+		return garantie;
+	}
+	public void setGarantie(Boolean garantie) {
+		this.garantie = garantie;
+	}
+	public Integer getFreqApprovisionnement() {
+		return freqApprovisionnement;
+	}
+	public void setFreqApprovisionnement(Integer freqApprovisionnement) {
+		this.freqApprovisionnement = freqApprovisionnement;
+	}
+	public Date getDateSignature() {
+		return dateSignature;
+	}
+	public void setDateSignature(Date dateSignature) {
+		this.dateSignature = dateSignature;
+	}
+	public int getDuree() {
+		return duree;
+	}
+	public void setDuree(int duree) {
+		this.duree = duree;
+	}
+	public String getCommentaire() {
+		return commentaire;
+	}
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+	public Boolean getFlag() {
+		return flag;
+	}
+	public void setFlag(Boolean flag) {
+		this.flag = flag;
+	}
+	public Set<Bouquet> getListeBouquets() {
+		return listeBouquets;
+	}
+	public void setListeBouquets(Set<Bouquet> listeBouquets) {
+		this.listeBouquets = listeBouquets;
+	}
 	public Integer getNumero() {
 		return numero;
 	}
