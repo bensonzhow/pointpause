eda1a447e3e597384757aa2e9038d43f0ba736d5
annayafi@gmail.com <annayafi@gmail.com@46d0faaf-50e8-9009-db50-26aeffe045a9>
2010-06-30 09:00:58 +0000

    début de commencement de premisses de gestion de la connexion et interdiction d'accès à certaines pages si on est pas connecté.

--- trunk/PointPausePresentation/src/fr/afcepf/ai77/g1/presentation/bean/ConnectionBean.java
+++ trunk/PointPausePresentation/src/fr/afcepf/ai77/g1/presentation/bean/ConnectionBean.java
@@ -1,8 +1,9 @@
 package fr.afcepf.ai77.g1.presentation.bean;
 
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import fr.afcepf.ai77.g1.facade.DTOFactory;
 import fr.afcepf.ai77.g1.metiers.dto.SessionDTO;
@@ -13,6 +14,13 @@ public class ConnectionBean {
 	
 	private String login;
 	private String mdp; 
+	//private String statut;
+	
+	/*public String getStatut() {
+		return statut;
+	}
+	public void setStatut(String pStatut) {
+		statut = pStatut;}*/
 	
 	public String getLogin() {
 		return login;
@@ -39,9 +47,22 @@ public class ConnectionBean {
 		
 		if (session==null) {
 			System.out.println("login failed");
+			
+			FacesContext context = FacesContext.getCurrentInstance();  
+			HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();  
+			HttpSession httpSession = request.getSession(false);  
+			
+			httpSession.setAttribute("session", null);		
+			//statut="probleme d'identifiants";
 			return "Failure";
 		}else{
 			System.out.println("login success");
+			FacesContext context = FacesContext.getCurrentInstance();  
+			HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();  
+			HttpSession httpSession = request.getSession(false);  
+			
+			httpSession.setAttribute("session", session);
+			
 			return "OK";	
 		}
 	}
