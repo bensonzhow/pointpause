19495e0be0d1457b0507120b7d9f80afb4483a74
annayafi <annayafi@gmail.com>
2010-07-03 11:50:07 +0000

    Les machines sont dans la liste pour créer un contrat

---
+++ PointPauseData/src/fr/afcepf/ai77/g1/persistence/entity/TypeAutomate.java
@@ -0,0 +1,25 @@
+package fr.afcepf.ai77.g1.persistence.entity;
+import java.io.Serializable;
+public class TypeAutomate implements Serializable{
+	private int id;
+	private String nom, description;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
