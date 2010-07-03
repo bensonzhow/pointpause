48d83818faa20d4f164ec2d99b7a0e94c4758c56
annayafi@gmail.com <annayafi@gmail.com@46d0faaf-50e8-9009-db50-26aeffe045a9>
2010-07-03 11:50:51 +0000

    pareil la combo machines se remplit avec une description ajax générée à droite

--- trunk/PointPausePresentation/src/fr/afcepf/ai77/g1/presentation/bean/ChoixContratBean.java
+++ trunk/PointPausePresentation/src/fr/afcepf/ai77/g1/presentation/bean/ChoixContratBean.java
@@ -8,6 +8,7 @@ import javax.faces.model.SelectItem;
 import fr.afcepf.ai77.g1.facade.DTOFactory;
 import fr.afcepf.ai77.g1.metiers.interfaces.IDonneesChoixContratDTO;
 import fr.afcepf.ai77.g1.persistence.entity.Formule;
+import fr.afcepf.ai77.g1.persistence.entity.TypeAutomate;
 
 
 /**
@@ -17,61 +18,95 @@ import fr.afcepf.ai77.g1.persistence.entity.Formule;
  */
 public class ChoixContratBean {
 	private List<SelectItem> formules = new ArrayList<SelectItem>();
+	private List<SelectItem> machines = new ArrayList<SelectItem>();
+	private List<TypeAutomate> machinesDispo = new ArrayList<TypeAutomate>();
+	private String selectedFormule;
+	private String selectedMachine;
+	public String getSelectedMachine() {
+		return selectedMachine;
+	}
 
-	
-  public List<SelectItem> getFormules() {
+
+
+	public void setSelectedMachine(String selectedMachine) {
+		this.selectedMachine = selectedMachine;
+	}
+
+	private List<Formule> formulesList;
+	public List<SelectItem> getMachines() {
+		return machines;
+	}
+
+	private String description;
+
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public List<SelectItem> getFormules() {
 		return formules;
 	}
-	public void setFormules(List<SelectItem> formules) {
-		this.formules = formules;
-	}
+	
 
-private String selectedFormule;
+	
 	public String getSelectedFormule() {
-	return selectedFormule;
-}
+		return selectedFormule;
+	}
 	public void setSelectedFormule(String selectedFormule) {
 		this.selectedFormule = selectedFormule;
 	}
-	
-private List<Formule> formulesList;
-
-
-	private String descriptionFormuleSelected;
-
-	
 
 	public String getDescriptionFormuleSelected() {
-		return descriptionFormuleSelected;
+		return description;
 	}
-
 	public void setDescriptionFormuleSelected(String descriptionFormuleSelected) {
-		this.descriptionFormuleSelected = descriptionFormuleSelected;
+		this.description = descriptionFormuleSelected;
 	}
 
+
+	
+
+public void getDescriptionMachine(){
+	for (TypeAutomate automate: machinesDispo)
+	{
+		if(automate.getNom().equals(this.selectedMachine))
+		{
+			description= "Description de la machine :" +selectedMachine + " : "+ automate.getDescription();
+		}
+	}
+}
 
 
 	public void getDescriptionFormule(){
-		
+
 		{
 			for (Formule formule : formulesList) {
 				if (formule.getLibelleFormule().equals(this.selectedFormule))
 				{
-					descriptionFormuleSelected= formule.getCommentaireFormule();
+					description= "Description de la formule "+ selectedFormule+ " : "+ formule.getCommentaireFormule();
 				}
 			}
-
-
 		}
-		
-		
-	//	descriptionFormuleSelected="lala";
+
+
+		//	descriptionFormuleSelected="lala";
 	}
-	
+
 	public ChoixContratBean(){
-		IDonneesChoixContratDTO  donneesSession = DTOFactory.getIDonneesChoixContratDTO();
-		 formulesList= donneesSession.getAllGeneral();
-		
+		IDonneesChoixContratDTO  donneesContrat = DTOFactory.getIDonneesChoixContratDTO();
+		formulesList= donneesContrat.getAllGeneral();
+
 		for (Formule formule : formulesList) {
 			SelectItem si = new SelectItem();
 			si.setLabel(formule.getLibelleFormule());
@@ -80,6 +115,15 @@ private List<Formule> formulesList;
 			formules.add(si);
 		}
 		
+		machinesDispo= donneesContrat.getAllMachines();
+		for (TypeAutomate machine : machinesDispo) {
+			SelectItem si = new SelectItem();
+			si.setLabel(machine.getNom());
+			si.setDescription(machine.getNom());
+			si.setValue(machine.getNom());
+			machines.add(si);
+		}
+
 
 
 	}
